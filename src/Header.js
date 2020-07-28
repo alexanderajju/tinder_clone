1b4b956d2f35ce7208101022ccdaa9d453bed9da
alexanderajju <alexanderajju@gmail.com>
2020-07-28 14:27:56 +0530

    Page routing

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
-import IconButton from'@material-ui/core/IconButton'
+import IconButton from'@material-ui/core/IconButton';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
@@ -10,13 +11,19 @@ function Header() {
             <IconButton>
             <PersonIcon className="header__icon" fontSize="large" />
             </IconButton>
-            
+            <Link to="/">
             <img 
             className="header__logo"
             src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="tinder logo"/>
+           
+            </Link>
+            
+           <Link to="/chat">
            <IconButton>
            <ForumIcon className="header__icon" fontSize="large" />
            </IconButton>
+           </Link>
+           
             
         </div>
     )
