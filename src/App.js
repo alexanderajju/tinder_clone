2e78b54978a27bfea0daa2854d22ad68f688871a
alexanderajju <alexanderajju@gmail.com>
2020-07-28 10:09:00 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/chat">
+            <h1>Iam a chatapp</h1>
+          </Route>
+          <Route path="/">
+            <h1>Iam a home page</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
